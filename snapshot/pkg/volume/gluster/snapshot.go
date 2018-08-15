e5d4247e42bf0c3204221a3a32ff6176259a1e52
zoues <zounengren@cmss.chinamobile.com>
2018-08-15 11:12:36 +0800

    instead info to info format

--- snapshot/pkg/volume/gluster/snapshot.go
+++ snapshot/pkg/volume/gluster/snapshot.go
@@ -71,7 +71,7 @@ func (h *glusterfsPlugin) SnapshotCreate(
 	if err != nil {
 		glog.Errorf("failed to create snapshot for volume :%v, out:%v, err: %v", volumePath, out, err)
 	}
-	glog.V(1).Info("snapshot %v created successfully", snapshotName)
+	glog.V(1).Infof("snapshot %v created successfully", snapshotName)
 	cmd = exec.Command(glusterfsBinary, "snapshot", "activate", snapshotName)
 	_, err = cmd.CombinedOutput()
 
